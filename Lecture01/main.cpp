1a20fe27e74794b226c007510e0b87e071951ae2
Ioan Crișan <ioan.crisan@kephas-software.com>
2025-03-14 08:12:15 +0200

    Added Lecture01

--- Lecture01/main.cpp
+++ Lecture01/main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 
+struct Point {
+    int x;
+    int y;
+};
+
+class Rectangle {
+public:
+    Rectangle(int width, int height) : width(width), height(height) {}
+
+    int getArea() {
+        return width * height;
+    }
+
+    int width;
+    int height;
+};
+
+int summe(int x, int y) {
+    return x + y + summe(x, y);
+}
+
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
+
+    auto sum = summe(1, 2);
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
     // <b>lang</b> variable name to see how CLion can help you rename it.
     auto lang = "C++";
+    auto k = 2;
     std::cout << "Hello and welcome to " << lang << "!\n";
+
+    Point p = {1, 2};
+    Rectangle r(3, 4);
 
     for (int i = 1; i <= 5; i++) {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
